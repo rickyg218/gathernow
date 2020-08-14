9d385723aea14c9bbc0e1e92a8b4b66f9fbe64f9
wild1k <cssndrchmbrln@gmail.com>
2020-08-13 22:46:41 -0700

    added the ant npm pkg and added the login screen form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBarMenu from "./components/NavBar/index";
+import { Button } from 'antd-mobile';
+import './App.css';
 
-import Home from './components/Home';
-import logIn from './components/logIn';
+import Home from './pages/Home';
+import logIn from './pages/LogIn';
 import Navigation from './components/Navigation';
-import CreateAccount from './components/CreateAccount';
+import CreateAccount from './pages/CreateAccount';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { NavBar, Icon } from 'antd-mobile';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
