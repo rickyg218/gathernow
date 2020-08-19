663117cdb0f7950ef2e49d2a4ad5443a14b1cca5
wild1k <cssndrchmbrln@gmail.com>
2020-08-19 11:16:33 -0700

    added the ugliest card in the world to my profile page for backend connectivity and fixed some warning issues

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Container from '../components/UserProfile/UserProfile';
-
+import Card from '../components/UserProfile/EventCard.js';
 import '../index';
-import Button from "../components/UserProfile/Card";
+// import Button from "../components/UserProfile/notification";
 
 export default function MyAccount() {
   return(
-    <>
+    <div>
     <h1>My account Page</h1>
    
       <Container/>
-      <Button/>
-       </>
+      <Card/>
+      {/* <Button/> */}
+       </div>
   )
 }
